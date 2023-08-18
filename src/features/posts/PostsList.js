d331e3e06a282848d85f3139532aee76ac4a75f0
wali55 <walisantunu@gmail.com>
2023-08-18 20:31:54 +0600

    optimized app

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,13 +1,9 @@
 import { useSelector } from 'react-redux';
-import {
-  selectAllPosts,
-  getPostsStatus,
-  getPostsError,
-} from './postsSlice';
+import { selectPostIds, getPostsStatus, getPostsError } from './postsSlice';
 import PostsExcerpt from './PostsExcerpt';
 
 const PostsList = () => {
-  const posts = useSelector(selectAllPosts);
+  const orderedPostIds = useSelector(selectPostIds);
   const postsStatus = useSelector(getPostsStatus);
   const error = useSelector(getPostsError);
 
@@ -15,20 +11,13 @@ const PostsList = () => {
   if (postsStatus === 'loading') {
     content = <p>'Loading...'</p>;
   } else if (postsStatus === 'succeeded') {
-    const orderedPosts = posts
-      .slice()
-      .sort((a, b) => b.date.localeCompare(a.date));
-    content = orderedPosts.map((post) => (
-      <PostsExcerpt key={post.id} post={post} />
+    content = orderedPostIds.map((postId) => (
+      <PostsExcerpt key={postId} postId={postId} />
     ));
   } else if (postsStatus === 'failed') {
     content = <p>{error}</p>;
   }
 
-  return (
-    <section>
-      {content}
-    </section>
-  );
+  return <section>{content}</section>;
 };
 export default PostsList;
